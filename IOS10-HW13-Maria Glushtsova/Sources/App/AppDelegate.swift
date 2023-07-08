9ca78b7f8628e54b9e2c69d4bbb0f0c5654905c7
MariaGlushtsova <glushtsova@tut.by>
2023-07-08 18:55:27 +0300

    Initial settings
    Создание папок в проектие, добавление SnapKit

---
+++ IOS10-HW13-Maria Glushtsova/Sources/App/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  IOS10-HW13-Maria Glushtsova
+//
+//  Created by Admin on 8.07.23.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
