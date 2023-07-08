3e812b2aedbb93a87450ad463f9605ef026df067
MariaGlushtsova <glushtsova@tut.by>
2023-07-08 18:29:47 +0300

    Initial Commit

---
+++ IOS10-HW13-Maria Glushtsova/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOS10-HW13-Maria Glushtsova
+//
+//  Created by Admin on 8.07.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
